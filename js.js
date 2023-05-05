484893471a14ddd1430805128e216a5fe22de836
Naiitiry <danchukroman@gmail.com>
2023-05-04 23:58:37 -0300

    cambios

--- js.js
+++ js.js
@@ -3,5 +3,10 @@ const displayValorActual = document.getElementById('valor-actual');
 const botonesNumeros = document.querySelectorAll('.numero');
 const botonesOperadores = document.querySelectorAll('.operador');
 
+const display = new Display(displayValorAnterior, displayValorActual);
 
-const calculadora = new Calculadora();
+botonesNumeros.forEach(boton =>{
+    boton.addEventListener('click',()=>{
+        display.agregarNumero(boton.innerHTML);
+    });
+});
