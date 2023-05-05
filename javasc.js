7f0294370367c6e2cd425e994a53229a6fe4fa66
Naiitiry <danchukroman@gmail.com>
2023-05-04 23:44:16 -0300

    cambios

--- javasc.js
+++
@@ -1,8 +0,0 @@
-const displayValorAnterior = document.getElementById('valor-anterior');
-const displayValorActual = document.getElementById('valor-actual');
-const botonesNumeros = document.querySelectorAll('.numero');
-const botonesOperadores = document.querySelectorAll('.operador');
-
-const calculadora = new Calculadora();
-
-console.log(calculadora.sumar(2,3))
