2811b4a6ab84cb500d39ace29115d2b16b5b4d57
Naiitiry <danchukroman@gmail.com>
2023-05-05 07:31:39 -0300

    actualización y finalización de calculadora

--- Display.js
+++ Display.js
@@ -3,15 +3,48 @@ class Display{
         this.displayValorActual = displayValorActual;
         this.displayValorAnterior = displayValorAnterior;
         this.calculadora= new Calculadora();
+        this.tipoOperacion = undefined;
         this.valorActual = '';
         this.valorAnterior = '';
+        this.signos = {
+            sumar: '+',
+            dividir:'/',
+            restar:'-',
+            multiplicar:'x'
+        }
     }
     agregarNumero(numero){
-        this.valorActual = numero;
+        if(numero === '.' && this.valorActual.includes('.')) return
+        this.valorActual = this.valorActual.toString() + numero.toString();
         this.imprimirValores();
     }
     imprimirValores(){
         this.displayValorActual.textContent = this.valorActual;
-        this.displayValorAnterior.textContent = this.valorAnterior;
+        this.displayValorAnterior.textContent = `${this.valorAnterior} ${this.signos[this.tipoOperacion] || ''}`;
+    }
+    borrar(){
+        this.valorActual = this.valorActual.toString().slice(0,-1);
+        this.imprimirValores();
+    }
+    borrarTodo(){
+        this.valorAnterior = '';
+        this.valorActual = '';
+        this.tipoOperacion = undefined;
+        this.imprimirValores();
+    }
+    computar(tipo){
+        this.tipoOperacion !== 'igual' && this.calcular();
+        this.tipoOperacion = tipo;
+        this.valorAnterior = this.valorActual || this.valorAnterior;
+        this.valorActual='';
+        this.imprimirValores();
+    }
+    calcular(){
+        //toma los datos que hay en el display y se los da a la Calculadora
+        const valorAnterior = parseFloat(this.valorAnterior);
+        const valorActual = parseFloat(this.valorActual);
+
+        if(isNaN(valorActual) || isNaN(valorAnterior)) return
+        this.valorActual = this.calculadora[this.tipoOperacion](valorAnterior, valorActual);
     }
 }
